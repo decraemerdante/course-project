f96543a5d8f8a50b6554b045fa865c6506fe9675
Dante De Craemer <dante.decraemer@stardekk.com>
2020-10-12 16:22:50 +0200

    init

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "This is a test", "https://realfood.tesco.com/media/images/RFO-1400x919-AsianSalmon-9a9cf566-eaad-4107-aa79-886ec53e6b31-0-1400x919.jpg"),
+    new Recipe("Test Recipe", "This is a test", "https://realfood.tesco.com/media/images/RFO-1400x919-AsianSalmon-9a9cf566-eaad-4107-aa79-886ec53e6b31-0-1400x919.jpg")
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
